7f0cee0a69db531767f698d3df410244c9a30d68
SmallBowl <721025@student.dusd.net>
2024-05-22 15:17:46 -0700

    Arena, Movement, Simple Enemy

--- FinalProjectFunnyBurger/Assets/Scripts/PlayerController.cs
+++ FinalProjectFunnyBurger/Assets/Scripts/PlayerController.cs
@@ -4,12 +4,23 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    
- 
+    private Rigidbody playerRb;
+    public float speed = 5.0f;
+
+    private void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        playerRb.maxLinearVelocity = speed;
+        
+    }
+
 
     // Update is called once per frame
     void Update()
     {
+        float forwardInput = Input.GetAxis("Vertical");
+        float sideInput = Input.GetAxis("Horizontal");
+        playerRb.AddForce(sideInput * speed, 0, forwardInput * speed); 
         
     }
 
