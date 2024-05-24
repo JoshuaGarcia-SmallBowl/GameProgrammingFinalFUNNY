8c2c3fb182b18f9581930e5a0fc68cef6e655a61
SmallBowl <721025@student.dusd.net>
2024-05-24 15:29:28 -0700

    Attacking, taking damage, dying, player and mushroom animations

--- FinalProjectFunnyBurger/Assets/Scripts/PlayerController.cs
+++ FinalProjectFunnyBurger/Assets/Scripts/PlayerController.cs
@@ -1,58 +1,146 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    
+
     public float speed = 5.0f;
     private CharacterController characterController;
     float horizontal;
     float vertical;
     Vector3 movement;
+
+    private Animator animator;
+    private int health = 100;
+    private bool movable = true;
+
+    private float atkStart;
+    private bool held;
+
+    private bool hurtable = true;
+
     private void Start()
     {
         characterController = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
+
+    }
+
+    void FixedUpdate()
+
+    {
+        if (movable)
+        {
+            horizontal = Input.GetAxisRaw("Horizontal");
+            vertical = Input.GetAxisRaw("Vertical");
+            Vector3 velo = characterController.velocity;
+
+            if (horizontal != 0 || vertical != 0)
+            {
+                movement = new Vector3(horizontal, 0f, vertical).normalized * speed * Time.deltaTime;
+
+                characterController.Move(movement);
+                animator.SetBool("Moving", true);
+
+
+            }
+            else
+            {
+                animator.SetBool("Moving", false);
+            }
+        }
+
+
+
+    }
+
+    private void Update()
+    {
+        if (movable)
+        {
+            //rotation
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            Debug.DrawRay(ray.origin, ray.direction, Color.yellow);
+            if (Physics.Raycast(ray, out hit))
+            {
+                Vector3 targetPosition = new Vector3(hit.point.x, transform.position.y, hit.point.z);
+                Quaternion rotation = Quaternion.LookRotation(targetPosition - transform.position);
+
+                transform.rotation = Quaternion.Lerp(transform.rotation, rotation, Time.deltaTime * 10.0f);
+
+            }
+
+            //attacking
+            if (Input.GetMouseButtonDown(0))
+            {
+                held = true;
+                atkStart = Time.time;
+
+            }
+            if (Input.GetMouseButtonUp(0))
+            {
+                if (held)
+                {
+                    float heldTime = Time.time - atkStart;
+                    if (heldTime > 0.5f)
+                    {
+                        Debug.Log("Ranged");
+                    }
+                    else
+                    {
+                        Debug.Log("Melee");
+                    }
+                }
+                held = false;
+            }
+        }
         
+
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (hurtable)
+        {
+            takeDamage(25);
+            StartCoroutine(Invincibility());
+        }
         
     }
 
-
-    
-    void FixedUpdate()
+    void takeDamage(int damage)
+    {
+        animator.SetBool("Damaged", true);
+            health -= damage;
+            Debug.Log("Health" + health);
+        if (health <= 0)
+        {
+            animator.SetBool("Dead", true);
+            movable = false;
+        }
         
+    }
+
+    System.Collections.IEnumerator Invincibility()
     {
         
-        horizontal = Input.GetAxisRaw("Horizontal");
-        vertical = Input.GetAxisRaw("Vertical");
-        Vector3 velo = characterController.velocity;
-        
-        if (horizontal != 0 || vertical != 0)
-        {
-            movement = new Vector3(horizontal, 0f, vertical).normalized * speed * Time.deltaTime;
 
-            characterController.Move(movement);
-            
-            
-        }
+        hurtable = false;
+        yield return new WaitForSeconds(1);
+        hurtable = true;
 
-
-        
-
-        //rotation
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        RaycastHit hit;
-        Debug.DrawRay(ray.origin, ray.direction, Color.yellow);
-        if (Physics.Raycast(ray, out hit))
-        {
-            Vector3 targetPosition = new Vector3(hit.point.x, transform.position.y, hit.point.z);
-            Quaternion rotation = Quaternion.LookRotation(targetPosition - transform.position);
-
-            transform.rotation = Quaternion.Lerp(transform.rotation, rotation, Time.deltaTime * 10.0f);
-
-        }
         
     }
+
+    public void damageEnd()
+    {
+        animator.SetBool("Damaged", false);
+    }
+
 
 
 }
